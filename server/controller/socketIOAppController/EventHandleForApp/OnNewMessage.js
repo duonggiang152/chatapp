26defb3c6b7d93b94bd501396bb815aeb111f7e9
duonggiang152 <62105435+duonggiang152@users.noreply.github.com>
2022-02-20 21:10:33 +0700

    feat: add new event

--- server/controller/socketIOAppController/EventHandleForApp/OnNewMessage.js
+++ server/controller/socketIOAppController/EventHandleForApp/OnNewMessage.js
@@ -4,11 +4,25 @@
 const eventEmitterSocketIOApp       = require("../eventsocketioapphandle");
 const IOServer                      = require("../../IOServer")
 const SocketManager                 = require("../SocketManager");
-const ChatMessage = require("../../../model/message");
+const ChatBox                       = require("../../../model/chatbox");
+const Logger = require("../../Logger/Logger");
 
 
  module.exports = () => {
-    eventEmitterSocketIOApp.on("new-message", (userID, cbID) => {
-        const boxChat = ChatMessage.getChatMessagebyID(messageID)
+    eventEmitterSocketIOApp.on("new-message",async (userID, messageInfo) => {
+        const boxMember = await ChatBox.getMemberOfChatBox(messageInfo.cbID)
+        const sendPacket = {
+            userID:messageInfo.userId,
+            message:messageInfo.message,
+            datetime:messageInfo.datetime
+        }
+        boxMember.forEach(async member => {
+            if(member.userID !== messageInfo.userId) {
+                const sockets = await SocketManager.getSocket(member.userID)
+                sockets.forEach(socket => {
+                    IOServer.io.to(socket).emit("new-message", sendPacket)
+                })
+            }
+        });
     }) 
  }
