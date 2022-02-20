e8944745bdf938f3c1f2e20b802f805c6054522a
duonggiang152 <62105435+duonggiang152@users.noreply.github.com>
2022-02-20 21:12:26 +0700

    feat: init new event

--- server/controller/socketIOAppController/InitEventHanddleSocketIOApp.js
+++ server/controller/socketIOAppController/InitEventHanddleSocketIOApp.js
@@ -1,10 +1,12 @@
 const InitNewFriendRequestNotification          = require("./EventHandleForApp/OnNewFriendRequestNotification")
 const InitNewAcceptFriendRequestNotification    = require("./EventHandleForApp/OnNewAcceptFriendRequestNotification")
+const InitNewMessageNotification                = require("./EventHandleForApp/OnNewMessage")
 const eventEmitterSocketIOApp                   = require("./eventsocketioapphandle")
 
 module.exports = () => {
     eventEmitterSocketIOApp.removeAllListeners()
     InitNewFriendRequestNotification()
     InitNewAcceptFriendRequestNotification()
+    InitNewMessageNotification()
 }
 
